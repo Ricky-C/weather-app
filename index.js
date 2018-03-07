8e1de23ebc1ae4dcf793f3f229fa60a7063117f8
Ricky-C <cabaethernet@gmail.com>
2018-03-07 01:26:21 -0500

    |change|JS Location|

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import Bootstrap from 'bootstrap/dist/css/bootstrap.min.css';
-import './index.css';
+import 'src/index.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
